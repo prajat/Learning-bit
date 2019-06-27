cb5ba335dc9c435b224e8be1ca348b8b14e00a9d
prajat <rajat.singh1084@gmail.com>
2019-06-27 14:39:01 +0530

    index.js changed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/App';
 
-// import styles
 import './styles.scss';
 
-// render
+
 ReactDOM.render( <App />, document.getElementById( 'app' ) );
