2e78e01ebd1130ab760ef978e03b8f3ee7ff5628
prajat <rajat.singh1084@gmail.com>
2019-06-28 13:14:02 +0530

    nav bar imported from dhrub bit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
-//import {Fragment} from 'react';
+import {Fragment} from 'react';
 import Header from './Header/index';
+import Navbar from '@bit/dhruv610.navbar.navbar';
 
 export class App extends Component{
     constructor(props){
@@ -9,8 +10,11 @@ export class App extends Component{
 
     render(){
         return(
-            
+            <Fragment>
+            {/* <Navbar links={[{name:"Rajat",link:"#",id:1}]} /> */}
             <Header />
+
+            </Fragment>
         
         );
     }
